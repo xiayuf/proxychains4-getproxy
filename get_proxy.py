317f2508b9addd0778775cbb9ba618ac818ac263
夏玉峰 <39935348@qq.com>
2018-01-11 14:09:07 +0800

    url proxy update

--- get_proxy.py
+++ get_proxy.py
@@ -5,7 +5,7 @@ import time
 import os
 from bs4 import BeautifulSoup
 
-url = "https://www.kuaidali.com/free/"
+url = "https://www.kuaidaili.com/free/"
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36'}
 
 def write_conf(ips,ports):
